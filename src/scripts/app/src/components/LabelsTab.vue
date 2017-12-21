6e3bee4cf826eb2070e9f1d36899996141f59f84
IvannMG <ivannm@theodo.fr>
2017-12-21 17:23:59 +0100

    add back the csv export button

--- src/scripts/app/src/components/LabelsTab.vue
+++ src/scripts/app/src/components/LabelsTab.vue
@@ -1,6 +1,12 @@
 <template>
   <div v-if="isActiveTab">
     <div class="tab-pane active">
+      <div v-if="hasCsvExport">
+        <a class="btn mirador__export-btn btn-success" :href="csvExport" download="epic.csv">
+          <i class="fa fa-file-excel-o" aria-hidden="true"></i>
+          Export in CSV
+        </a>
+      </div>
       <div class="epic__stats" v-for="label in labels">
         <div class="label-container">
           <div class="column__informations py-2">
@@ -35,7 +41,10 @@ export default {
       }
     },
     isActiveTab: Boolean,
-    hasCSVExport: Boolean,
+    hasCsvExport: {
+      type: Boolean,
+      default: false,
+    }
   },
   computed: {
     maxCompletionBarSize: function() {
@@ -47,8 +56,8 @@ export default {
       }))
     },
     csvExport: function() {
-      let csv = Object.keys(this.epics[0]).join() + "\n"
-      this.epics.forEach((epic) => {
+      let csv = Object.keys(this.labels[0]).join() + "\n"
+      this.labels.forEach((epic) => {
         csv += Object.values(epic) + "\n"
       })
       return 'data:text/csv;charset=utf-8,' + encodeURI(csv)
--- src/scripts/app/src/components/LabelsTabs.vue
+++ src/scripts/app/src/components/LabelsTabs.vue
@@ -24,6 +24,7 @@
       <LabelsTab
         :labels="epics"
         :is-active-tab="activeTab === 'epic-tab'"
+        :has-csv-export="true"
       />
       <LabelsTab
         :labels="otherLabels"
